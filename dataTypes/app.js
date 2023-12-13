adb7218d1a8dcc8dd1e6801fa2ba11bd36038482
Bekzat Kamen <bekzatkamen@MacBook-Air-Bekzat.local>
2023-12-13 21:20:03 +0600

    Переменные и типы данных

---
+++ dataTypes/app.js
@@ -0,0 +1,16 @@
+var name = 'Begzat';
+console.log(name);
+let age = 30;
+console.log(age);
+
+const male = 'male';
+console.log(male);
+
+user = {
+    name: 'Begzat',
+    age: 26,
+    skill: 'FullStack web developer'
+};
+
+console.table(user);
+
